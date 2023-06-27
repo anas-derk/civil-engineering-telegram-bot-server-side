2a1903575df75b9acea940b2b94b545a4ba8a9e2
anas derk <anas.derk2020@gmail.com>
2023-06-27 16:49:58 -0700

    Continue With Programming Bot ( Restructure Code)

---
+++ telegram-bot/functions.js
@@ -0,0 +1,50 @@
+const axios = require("axios");
+
+async function getAllFiles(apiRoute) {
+    const res = await axios.get(`${BASE_API_URL}/${apiRoute}`);
+    const data = await res.data;
+    return data;
+}
+
+async function getCustomFile(year, season, apiRoute, chatId) {
+    const res = await axios.get(`${BASE_API_URL}${apiRoute}?year=${year}&season=${season}`);
+    const data = await res.data;
+    if (data.length === 0) {
+        await bot.sendMessage(chatId, "عذراً لا توجد ملفات حالياً");
+    } else {
+        const fileUrl = `${BASE_API_URL}/${data[0].fileUrl}`;
+        const response = await axios.get(fileUrl, { responseType: 'arraybuffer' });
+        await bot.sendDocument(chatId, response.data);
+    }
+}
+
+async function processUserChoices(chatId, choises) {
+    try {
+        switch (choises.service) {
+            case "medallion": {
+                await getCustomFile(choises.year, choises.season, "/medallions/custom-medallion-file", chatId);
+                break;
+            }
+            case "courses": {
+                await getCustomFile(choises.year, choises.season, "/courses/custom-course-file", chatId);
+                break;
+            }
+            case "lectures": {
+                await getCustomFile(choises.year, choises.season, "/lectures/custom-lecture-file", chatId);
+                break;
+            }
+            default: {
+                console.log(err);
+            }
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports = {
+    getAllFiles,
+    getCustomFile,
+    processUserChoices,
+}
---
+++ telegram-bot/main.js
@@ -0,0 +1,92 @@
+/* Start Handle Telegram Bot Events */
+
+const token = "6208009889:AAHn2kqO2IS2ojG-dYpNpOBcOCp1pWgvAXo";
+
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(token, { polling: true });
+
+const channelId = "@Civil_Engineering_TU";
+
+const channeURL = "https://t.me/Civil_Engineering_TU";
+
+let userChoises = {};
+
+const { getAllFiles, getCustomFile, processUserChoices } = require("./functions");
+
+bot.onText(/\/start/, async (msg) => {
+    const chatId = msg.chat.id;
+    const userId = msg.from.id;
+    await bot.sendMessage(chatId, "مرحباً بك في البوت الخاص بنا");
+    const memberInfo = await bot.getChatMember(channelId, userId);
+    if (memberInfo.status === "left") {
+        await bot.sendMessage(chatId, "عذراً أنت لست مشتركاً في قناتنا الرجاء الاشترك على الرابط : " + channeURL);
+    } else {
+        await bot.sendMessage(chatId, "الرجاء اختيار السنة", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: "سنة أولى", callback_data: "first-year" }],
+                    [{ text: "سنة ثانية", callback_data: "second-year" }],
+                    [{ text: "سنة ثالثة", callback_data: "third-year" }],
+                    [{ text: "سنة رابعة", callback_data: "fourth-year" }],
+                    [{ text: "سنة خامسة", callback_data: "fifth-year" }],
+                ],
+            }
+        })
+    }
+});
+
+bot.on("callback_query", async (query) => {
+    const selectedValue = query.data;
+    const chatId = query.message.chat.id;
+    if (!userChoises[chatId]) {
+        userChoises[chatId] = { year: null, season: null, service: null, fileId: null };
+    }
+    if (selectedValue === "first-year"
+        || selectedValue === "second-year"
+        || selectedValue === "third-year"
+        || selectedValue === "fourth-year"
+        || selectedValue === "fifth-year"
+    ) {
+        userChoises[chatId].year = selectedValue;
+        await bot.sendMessage(chatId, "الرجاء اختيار الفصل", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: "فصل أول", callback_data: "first-season" }],
+                    [{ text: "فصل ثاني", callback_data: "second-season" }],
+                ],
+            }
+        });
+    } else if (selectedValue === "first-season" || selectedValue === "second-season") {
+        userChoises[chatId].season = selectedValue;
+        await bot.sendMessage(chatId, "الرجاء اختيار الخدمة المطلوبة", {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: "محاضرات", callback_data: "lectures" }],
+                    [{ text: "دورات", callback_data: "courses" }],
+                    [{ text: "نوط", callback_data: "medallion" }],
+                ],
+            }
+        });
+    } else if (selectedValue === "medallion") {
+        userChoises[chatId].service = selectedValue;
+        const data = await getAllFiles(`${BASE_API_URL}/all-courses`);
+        console.log(data);
+        // await bot.sendMessage(chatId, "الرجاء اختيار الخدمة المطلوبة", {
+        //     reply_markup: {
+        //         inline_keyboard: [
+        //             [{ text: "محاضرات", callback_data: "lectures" }],
+        //             [{ text: "دورات", callback_data: "courses" }],
+        //             [{ text: "نوط", callback_data: "medallion" }],
+        //         ],
+        //     }
+        // });
+    }
+    else {
+        getAllFiles(`${BASE_API_URL}/all-courses`)
+        userChoises[chatId].fileId = selectedValue;
+        processUserChoices(chatId, userChoises[chatId]);
+    }
+});
+
+/* End Handle Telegram Bot Events */
