a4eea75c083d522cfa486aa00878d275a575ddbc
anas derk <anas.derk2020@gmail.com>
2023-07-04 13:22:16 -0700

    Change BASE API URL

--- telegram-bot/data.js
+++ telegram-bot/data.js
@@ -2,9 +2,9 @@ const channelId = "@Civil_Engineering_TU";
 
 const channeURL = "https://t.me/Civil_Engineering_TU";
 
-// const BASE_API_URL = "https://api.civil-engineering-tu.online";
+const BASE_API_URL = "https://api.civil-engineering-tu.online";
 
-const BASE_API_URL = "http://localhost:4000";
+// const BASE_API_URL = "http://localhost:4000";
 
 let userChoises = {};
 
